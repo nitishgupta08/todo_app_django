0e2d02274940f8cc502d055370b606e052e47bb4
Nitish Kumar Gupta <nitishkg.88@gmail.com>
2023-07-18 13:53:22 +0530

    updated script.py

--- script.py
+++ script.py
@@ -1,3 +1,7 @@
+"""
+Script to populate db with 50 tags and 25 todos
+"""
+
 import os
 import random
 import sys
@@ -17,17 +21,15 @@ from todo.models import Tag, Todo
 fake = Faker()
 
 status = ['OPEN', 'WORKING', 'DONE', "OVERDUE"]
-des = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, amet quod dolore debitis inventore sequi aliquam. Beatae in, nulla animi praesentium dolorem saepe molestiae ducimus amet vitae aliquam iste ipsa maiores nesciunt eum voluptatum qui quibusdam illum eaque nostrum similique eligendi nisi. Itaque vero neque nihil, nostrum maiores laboriosam molestiae?"
+des = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, amet quod dolore debitis inventore sequi aliquam. Beatae in, nulla animi praesentium dolorem saepe molestiae ducimus amet vitae aliquam iste ipsa maiores nesciunt eum " \
+      "voluptatum qui quibusdam illum eaque nostrum similique eligendi nisi. Itaque vero neque nihil, nostrum maiores laboriosam molestiae?"
 
-for i in range(1,51):
+for i in range(1, 51):
     Tag.objects.create(name=f"tag {i}")
-
 
 tags = list(Tag.objects.all())
 
-
-
-for i in range(1,25):
-    todo_tags = random.choices(tags, k=random.choice([1,51]))
-    my_todo = Todo.objects.create(title=f'todo {i}', due_date=fake.date_between(start_date='today', end_date='+30d'), status=random.choice(status), description = des, high_priority=random.getrandbits(1))
+for i in range(1, 25):
+    todo_tags = random.choices(tags, k=random.choice([1, 51]))
+    my_todo = Todo.objects.create(title=f'todo {i}', due_date=fake.date_between(start_date='today', end_date='+30d'), status=random.choice(status), description=des, high_priority=random.getrandbits(1))
     my_todo.tags.set(todo_tags)
