8905263efdd8dcaaf4ffa67ced46370b2ffb8adf
Nitish Kumar Gupta <nitishgupta@Nitishs-MacBook-Air.local>
2023-08-14 23:25:01 +0530

    testing pr workflow #2

--- test.py
+++ test.py
@@ -54,5 +54,51 @@ def main():
     print("Prime numbers from 1 to 100:", prime_numbers)
 
 
+x = {  'a':37,'b':42,
+
+'c':927}
+
+x = 123456789.123456789E123456789
+
+if very_long_variable_name is not None and \
+ very_long_variable_name.field > 0 or \
+ very_long_variable_name.is_debug:
+ z = 'hello '+'world'
+else:
+ world = 'world'
+ a = 'hello {}'.format(world)
+ f = rf'hello {world}'
+if (this
+and that): y = 'hello ''world'#FIXME: https://github.com/psf/black/issues/26
+class Foo  (     object  ):
+  def f    (self   ):
+    return       37*-2
+  def g(self, x,y=42):
+      return y
+def f  (   a: List[ int ]) :
+  return      37-a[42-u :  y**3]
+def very_important_function(template: str,*variables,file: os.PathLike,debug:bool=False,):
+    """Applies `variables` to the `template` and writes to `file`."""
+    with open(file, "w") as f:
+     ...
+
+custom_formatting = [
+    0,  1,  2,
+    3,  4,  5,
+    6,  7,  8,
+]
+
+regular_formatting = [
+    0,  1,  2,
+    3,  4,  5,
+    6,  7,  8,
+]
+
+
 if __name__ == "__main__":
+    print('black test')
     main()
+    print('Hello world')
+
+
+
