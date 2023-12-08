f84f2bdee63b31de6c448e076e5867cd45e065ed
Nitish Kumar Gupta <57590993+nitishgupta08@users.noreply.github.com>
2023-12-08 08:25:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,11 +4,7 @@ x = {"a": 37, "b": 42, "c": 927}
 
 x = 123456789.123456789e123456789
 
-if (
-    very_long_variable_name is not None
-    and very_long_variable_name.field > 0
-    or very_long_variable_name.is_debug
-):
+if very_long_variable_name is not None and very_long_variable_name.field > 0 or very_long_variable_name.is_debug:
     z = "hello " + "world"
 else:
     world = "world"
